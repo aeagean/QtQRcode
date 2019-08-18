fdf4aed2bc48932e4995240c54c350ad12a2b84b
Strong <982381240@qq.com>
2019-08-18 22:37:18 +0800

    完善代码

--- main.cpp
+++ main.cpp
@@ -3,17 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    // Create the QR code
+    /* Create the QR code */
     QRCode qrcode;
-    uint8_t qrcodeData[qrcode_getBufferSize(3)];
+    uint8_t qrcodeData[qrcode_getBufferSize(3)] = {0};
     qrcode_initText(&qrcode, qrcodeData, 3, 0, "http://weixin.qq.com/r/p0xudjXEUmgtrXEV9xm1");
 
-    // Top quiet zone
     QString result;
     for (uint8_t y = 0; y < qrcode.size; y++) {
-        // Each horizontal module
+        /* Each horizontal module */
         for (uint8_t x = 0; x < qrcode.size; x++) {
-            // Print each module (UTF-8 \u2588 is a solid block)
+            /* Print each module (UTF-8 \u2588 is a solid block) */
             result += qrcode_getModule(&qrcode, x, y) ? QString("\u2588\u2588") : QString("  ");
         }
 
